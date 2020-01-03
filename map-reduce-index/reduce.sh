60293249d1cb37e8aef9c1963955ecd457f380ba
Felix Van der Jeugt <felix.vanderjeugt@posteo.net>
2020-01-03 10:55:25 +0100

    Add more complete options and use correct grep in pipeline

--- map-reduce-index/reduce.sh
+++ map-reduce-index/reduce.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-INTDIR=../small/intermediate
-TABDIR=../small/tables
+TABDIR="/user/data/gent/gvo000/gvo00038/vsc41079/data/tables"
+INTDIR="/kyukon/scratch/gent/vo/000/gvo00038/intermediate/"
 ACIDS=ACDEFGHIKLMNPQRSTVWY
 zcat $INTDIR/sequence_taxon.[$ACIDS].tsv.gz | umgap buildindex > $TABDIR/index
