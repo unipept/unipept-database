1852cc8e98196fc66afeccbd3a89540051609af5
Felix Van der Jeugt <felix.vanderjeugt@posteo.net>
2020-07-13 11:18:57 +0200

    Use UMGAP to aggregate kmers for the UMGAP index
    Also add a monitoring script to keep an eye on the frequency of the
    different ranks in the index

---
+++ frequency.py
@@ -0,0 +1,33 @@
+import sys
+from collections import defaultdict
+
+order = [
+	["subspecies", "varietas", "forma"],
+	["species"],
+	["genus", "subgenus", "species group", "species subgroup"],
+	["family", "subfamily", "tribe", "subtribe"],
+	["order", "suborder", "infraorder", "parvorder", "superfamily"],
+	["class", "subclass", "infraclass", "superorder"],
+	["phylum", "subphylum", "superclass"],
+	["superkingdom", "kingdom", "subkingdom", "superphylum"],
+	["no rank"]
+]
+
+counts = defaultdict(int)
+count = 0
+for line in sys.stdin:
+	counts[line.strip()] += 1
+	count += 1
+
+	if count > 1000:
+		count = 0
+		grouped = { group[0]: sum(counts[rank] for rank in group) for group in order }
+		maximum = max(value for key, value in grouped.items())
+		with open(sys.argv[1], 'w') as f:
+			for group in order:
+				#print(f'{group[0] + ":":15} {"-" * (100 * grouped[group[0]] // maximum)} ({grouped[group[0]]:,})', file=f)
+				print('{0:15} {1} ({2:,})'.format(
+					group[0] + ":",
+					"-" * (100 * grouped[group[0]] // maximum),
+					grouped[group[0]]
+				), file=f)
--- partial-index.sh
+++ partial-index.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 JAR=target/unipept-0.0.1-SNAPSHOT.jar
 PAC=org.unipept.tools
-TABDIR=index-creation
+TABDIR=data/tables
 ACIDS=ACDEFGHIKLMNPQRSTVWY
-
-{
-	start="$(date +%s)"
-	while sleep 30s; do
-		now="$(date +%s)"
-		printf '%s\t%d\t%d\n' "$(date -Is)" "$((now - start))" "$(df --output=used /data/tempfs | tail -1 | tr -d ' ')"
-	done > memtrace
-} &
-memtrace="$!"
 
 for PREFIX in A C D E F G H I K L M N P Q R S T V W Y; do
 	pv -N $PREFIX $TABDIR/uniprot_entries.tsv.gz \
@@ -23,8 +14,8 @@ for PREFIX in A C D E F G H I K L M N P Q R S T V W Y; do
 		| LC_ALL=C sort --parallel=16 -S 10G -T /data/tempfs \
 		| sed "s/^/$PREFIX/"
 done \
-		| java -Xms3g -Xmx3g -cp ${JAR} ${PAC}.LineagesSequencesTaxons2LCAs <(zcat $TABDIR/lineages.tsv.gz) \
+		| umgap joinkmers $TABDIR/taxons.tsv \
+		| tee >(cut -d'	' -f3 | python frequency.py $TABDIR/frequencies.txt) \
+		| cut -d'	' -f1,2 \
 		| umgap buildindex \
 		> $TABDIR/9-mer.index
-
-kill "$memtrace"
