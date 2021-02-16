b63894163ba938f0061b9059b18bd64b98bcc099
Felix Van der Jeugt <felix.vanderjeugt@posteo.net>
2021-02-16 19:30:49 +0100

    clear out makefile remnants

--- map-reduce-index/reduce.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-TABDIR="/user/data/gent/gvo000/gvo00038/vsc41079/data/tables"
-INTDIR="/kyukon/scratch/gent/vo/000/gvo00038/intermediate/"
-ACIDS=ACDEFGHIKLMNPQRSTVWY
-zcat $INTDIR/sequence_taxon.[$ACIDS].tsv.gz | umgap buildindex > $TABDIR/index
