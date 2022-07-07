f8b077b181dd9ee4d26e8afbf19bb47c5fd2b980
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2022-07-07 15:52:31 +0200

    Add new virtual tables to static database

---
+++ workflows/static_database/init_virtual_tables.sql
@@ -0,0 +1,14 @@
+-- This file provides some code to initialize virtual tables in our Unipept static database that aid to drastically
+-- improve query performance in specific situations (such as looking for taxa that contain a specific text in their
+-- name).
+
+-- First we have to create a new virtual table. Afterwards we can populate this table with content from the pre-existing
+-- taxon table.
+CREATE VIRTUAL TABLE `virtual_taxons` USING fts5 (
+    id UNINDEXED,
+    name,
+    rank
+);
+
+-- Now, populate this table using the data that's already present in the taxons table.
+INSERT INTO `virtual_taxons` SELECT id, name, rank FROM `taxons`;
