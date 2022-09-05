3d4a2c53b6fc028601cf0b300727d7458b56049f
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2022-09-05 15:15:05 +0200

    Add -x for debugging

--- scripts/build_database.sh
+++ scripts/build_database.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-set -eo pipefail
+set -exo pipefail
 
 # All references to an external script should be relative to the location of this script.
 # See: http://mywiki.wooledge.org/BashFAQ/028
