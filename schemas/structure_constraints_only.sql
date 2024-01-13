362327e3641e651ac57454acab2287b603cd6d44
stijndcl <declercq.stijn@outlook.com>
2024-01-13 18:12:41 +0100

    Add constraint-only schema

---
+++ schemas/structure_constraints_only.sql
@@ -0,0 +1,28 @@
+ALTER TABLE unipept.taxons
+    ADD PRIMARY KEY (id);
+ALTER TABLE unipept.uniprot_entries
+    ADD PRIMARY KEY (id);
+ALTER TABLE unipept.ec_numbers
+    ADD PRIMARY KEY (id);
+ALTER TABLE unipept.go_terms
+    ADD PRIMARY KEY (id);
+ALTER TABLE unipept.interpro_entries
+    ADD PRIMARY KEY (id);
+ALTER TABLE unipept.lineages
+    ADD PRIMARY KEY (taxon_id);
+ALTER TABLE unipept.sequences
+    ADD PRIMARY KEY (id);
+ALTER TABLE unipept.datasets
+    ADD PRIMARY KEY (id);
+ALTER TABLE unipept.dataset_items
+    ADD PRIMARY KEY (id);
+ALTER TABLE unipept.dataset_items
+    ADD CONSTRAINT fk_dataset_items_datasets
+        FOREIGN KEY (dataset_id)
+            REFERENCES unipept.datasets (id)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION;
+ALTER TABLE unipept.go_cross_references
+    ADD PRIMARY KEY (id);
+ALTER TABLE unipept.interpro_cross_references
+    ADD PRIMARY KEY (id);
