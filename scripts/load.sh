a255ada87ed08d9782bed109775e86be5949ec79
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2024-03-14 12:34:40 +0100

    Update gzip -> lz4 in load file

--- scripts/load.sh
+++ scripts/load.sh
@@ -17,7 +17,7 @@ do
     print $file
     tbl=`echo $file | sed "s/.tsv.lz4//"`
     echo "zcatting - LOAD DATA LOCAL INFILE '$file' INTO TABLE $tbl"
-    zcat $file | mariadb  --local-infile=1 -uunipept -punipept $db -e "LOAD DATA LOCAL INFILE '/dev/stdin' INTO TABLE $tbl;SHOW WARNINGS" 2>&1
+    lz4cat $file | mariadb  --local-infile=1 -uunipept -punipept $db -e "LOAD DATA LOCAL INFILE '/dev/stdin' INTO TABLE $tbl;SHOW WARNINGS" 2>&1
 done
 print "done"
 
