d6369b08e433ae91f69e60daa9aefeab819b7b8b
stijndcl <declercq.stijn@outlook.com>
2024-04-19 18:08:50 +0200

    Fix typo

--- scripts/parallel_index.sh
+++ scripts/parallel_index.sh
@@ -12,7 +12,7 @@ add_index() {
     local column_name=$2
 
     # Execute the "add index" statement
-    PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USER" -e "CREATE INDEX idx_$column_name ON $DB_NAME.$table_name($column_name);" &
+    PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USER" -c "CREATE INDEX idx_$column_name ON $DB_NAME.$table_name($column_name);" &
 }
 
 # List of tables and columns for which you want to add indexes
