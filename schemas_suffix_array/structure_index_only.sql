e755ce8a7d5057de5f8e46b8de6a63fbb54a1fc4
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2025-03-27 09:02:51 +0100

    Add FULLTEXT indices for uniprot_entries table

--- schemas_suffix_array/structure_index_only.sql
+++ schemas_suffix_array/structure_index_only.sql
@@ -8,6 +8,10 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 ALTER TABLE uniprot_entries ADD INDEX fk_uniprot_entries_taxons (taxon_id ASC);
 ALTER TABLE uniprot_entries ADD UNIQUE INDEX idx_uniprot_entries_accession (uniprot_accession_number ASC);
 
+SET GLOBAL innodb_ft_enable_stopword = OFF;
+ALTER TABLE uniprot_entries ADD FULLTEXT(name);
+ALTER TABLE uniprot_entries ADD FULLTEXT(uniprot_accession_number);
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
